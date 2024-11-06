fa0671f46587d1b17a6a6456302574adfdba0834
rodguit <rodguitarDev@outlook.com>
2024-11-06 00:53:32 -0300

    adicionando descrição e tamanho.

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 
 restaurante_praca = Restaurante('praça', 'Gourmet')
-restaurante_praca.receber_avaliacao('Gui', 10)
-restaurante_praca.receber_avaliacao('Lais', 8)
-restaurante_praca.receber_avaliacao('Dani', 2)
+bebida_suco = Bebida('Suco de Melancia', 5.0,'grande')
+prato_paozinho = Prato('Paozinho', 2.00,'O melhor pão da cidade')
+restaurante_praca.adicionar_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_no_cardapio(prato_paozinho)
+
 
 
 def main():
-    Restaurante.listar_restaurantes()
+    restaurante_praca.exibir_cardapio
 
 
 if __name__=='__main__':
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,14 +1,16 @@
 from modelos.avaliacao import Avaliacao
+from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Restaurante:
     restaurantes = []
     
     def __init__(self,nome,categoria):
-     self._nome = nome.title()
-     self.categoria = categoria.upper()
-     self._ativo = False
-     self._avaliacao = []
-     Restaurante.restaurantes.append(self)
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
+        self._avaliacao = []
+        self._cardapio = []
+        Restaurante.restaurantes.append(self)
      
     def __str__(self):
         return f'{self._nome} | {self.categoria}'
@@ -28,8 +30,8 @@ class Restaurante:
     
     def receber_avaliacao(self, cliente, nota):
         if 0 < nota <=5:
-           avaliacao = Avaliacao(cliente, nota)
-           self._avaliacao.append(avaliacao)
+            avaliacao = Avaliacao(cliente, nota)
+            self._avaliacao.append(avaliacao)
     
     @property
     def media_avaliacoes(self):
@@ -39,6 +41,19 @@ class Restaurante:
         quantidade_de_notas = len(self._avaliacao)
         media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
-        
-        
-
+   
+    def adicionar_no_cardapio(self,item):
+        if isinstance(item,ItemCardapio):
+            self._cardapio.append(item)
+            
+    @property
+    def exibir_cardapio(self):
+        print(f'Cardapio do restaurante {self._nome}\n')
+        for i,item in enumerate(self._cardapio,start=1):
+            if hasattr(item,'descricao'):
+                mensagem_prato = f'{i}. Nome:{item._nome} | Preço: R${item._preco} | Descrição: {item.descricao}'
+                print(mensagem_prato)
+            else:
+                mensagem_bebida = f'{i}. Nome:{item._nome} | Preço: R${item._preco} | Tamanho: {item.tamanho}'
+                print(mensagem_bebida)
+                
